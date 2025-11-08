715df606a8b8f45156733e653a2fd4b9ea621da7
Zakhar Savchyn <zakhar.savchyn.dev@gmail.com>
2025-11-08 11:16:37 +0200

    Add Docker image and container resources for Nginx

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,37 @@ resource "docker_volume" "web_content" {
     value = var.project_name
   }
 }
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.29.3-alpine"
+  keep_locally = false
+}
+
+
+resource "docker_container" "nginx" {
+  name  = "${var.project_name}-nginx"
+  image = docker_image.nginx.image_id
+
+  ports {
+    internal = 80
+    external = var.host_port
+  }
+
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+
+  volumes {
+    volume_name    = docker_volume.web_content.name
+    container_path = "/var/www/html"
+  }
+
+  restart = "unless-stopped"
+
+  labels {
+    label = "project"
+    value = var.project_name
+  }
+
+  depends_on = []
+}
