585a3ce8e01f11a4079705c542eb128d48686441
Zakhar Savchyn <zakhar.savchyn.dev@gmail.com>
2025-11-08 11:22:07 +0200

    Add outputs for Nginx container name, health check URL, and Docker network name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,14 @@
-# TODO
+output "nginx_container_name" {
+  description = "Name of the nginx container"
+  value       = docker_container.nginx.name
+}
+
+output "healthz_url" {
+  description = "Health check endpoint URL"
+  value       = "http://localhost:${var.host_port}/healthz"
+}
+
+output "network_name" {
+  description = "Docker network name"
+  value       = docker_network.app_network.name
+}
